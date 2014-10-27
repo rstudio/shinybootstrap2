24fc63b321596e23279db233de1704453ca43836
Winston Chang <winston@stdout.org>
2014-10-27 13:07:25 -0500

    Add withBoostrap2 function

--- R/shinyBootstrap2.R
+++ R/shinyBootstrap2.R
@@ -5,3 +5,17 @@
 #' @docType package
 #' @import htmltools jsonlite
 NULL
+
+
+#' @export
+withBootstrap2 <- function(expr, env = parent.frame()) {
+  expr <- substitute(expr)
+
+  # Copy everything from the shinyBootstrap2 environment to a new environment
+  # which is a child of the calling environment.
+  bs2env <- list2env(as.list(bs2env), parent = env)
+
+  eval(expr, bs2env)
+}
+
+bs2env <- environment()
