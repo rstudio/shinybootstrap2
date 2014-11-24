f433f60e33e46f91019d5fb38e9ee1aedb27fc33
Winston Chang <winston@stdout.org>
2014-11-24 11:29:05 -0600

    Add dataTableOutput

---
+++ R/output-datatable.R
@@ -0,0 +1,42 @@
+dataTableDependency <- list(
+  htmlDependency(
+    "datatables", "1.10.2",
+    c(file = system.file("www/datatables", package = "shinyBootstrap2")),
+    script = "js/jquery.dataTables.min.js"
+  ),
+  htmlDependency(
+    "datatables-bootstrap", "1.10.2",
+    c(file = system.file("www/datatables", package = "shinyBootstrap2")),
+    stylesheet = c("css/dataTables.bootstrap.css", "css/dataTables.extra.css"),
+    script = "js/dataTables.bootstrap.js"
+  )
+)
+
+#' Create a table output element
+#'
+#' Render a \code{\link{renderDataTable}} within an application page.
+#' \code{renderDataTable} uses the DataTables Javascript library to create an
+#' interactive table with more features.
+#'
+#' @param outputId output variable to read the table from
+#' @return A table output element that can be included in a panel
+#'
+#' @examples
+#' \donttest{
+#' library(shiny)
+#' shinyApp(
+#'   ui = bootstrapPage(
+#'     dataTableOutput('table')
+#'   ),
+#'   server = function(input, output) {
+#'     output$table <- renderDataTable(iris)
+#'   }
+#' )
+#' }
+#' @export
+dataTableOutput <- function(outputId) {
+  attachDependencies(
+    div(id = outputId, class="shiny-datatable-output"),
+    dataTableDependency
+  )
+}
